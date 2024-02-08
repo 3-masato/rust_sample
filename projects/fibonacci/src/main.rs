4fe8453b670e402cb7016d3a3e9a844f52b9d162
3-masato <lzxqt0@gmail.com>
2024-02-08 20:32:52 +0900

    Updated

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,12 +1,29 @@
+use std::io;
+
 fn fibonacci(x: u64) -> u64 {
     return match x {
         0 => 0,
-        1 => 1,
+        1 | 2 => 1,
         _ => fibonacci(x - 2) + fibonacci(x - 1),
     };
 }
 
 fn main() {
-    let num = fibonacci(50);
-    println!("fibonacci: {}", num);
+    println!("Calculate the Nth Fibonacci number: ");
+
+    loop {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        let num: u64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+
+        println!("The {}th Fibonacci number is {}", num, fibonacci(num));
+        break;
+    }
 }
