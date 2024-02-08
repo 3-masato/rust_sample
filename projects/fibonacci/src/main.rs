5f204df5278e73f7be1441643b47f9fc397844f8
3-masato <lzxqt0@gmail.com>
2024-02-08 20:55:42 +0900

    memorization

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,10 +1,14 @@
+use std::collections::HashMap;
 use std::io;
 
-fn fibonacci(x: u64) -> u64 {
-    return match x {
-        0 => 0,
-        1 | 2 => 1,
-        _ => fibonacci(x - 2) + fibonacci(x - 1),
+fn fibonacci(x: u64, memo: &mut HashMap<u64, u64>) -> u64 {
+    return match memo.get(&x) {
+        None => {
+            let n = fibonacci(x - 2, memo) + fibonacci(x - 1, memo);
+            memo.insert(x, n);
+            n
+        }
+        _ => memo[&x],
     };
 }
 
@@ -23,7 +27,10 @@ fn main() {
             }
         };
 
-        println!("The {}th Fibonacci number is {}", num, fibonacci(num));
+        let mut memo: HashMap<u64, u64> = HashMap::new();
+        memo.insert(0, 0);
+        memo.insert(1, 1);
+        println!("The {}th Fibonacci number is {}", num, fibonacci(num, &mut memo));
         break;
     }
 }
