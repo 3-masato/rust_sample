eec9f86f0efdcf070d29cee75d6f7d5b94d57616
3-masato <lzxqt0@gmail.com>
2024-02-10 18:20:04 +0900

    Added fizzbuzz test

--- projects/fizzbuzz/src/main.rs
+++ projects/fizzbuzz/src/main.rs
@@ -24,3 +24,46 @@ fn main() {
         println!("{}: {}, {}", x, fizzbuzz_basic(x), fizzbuzz_match(x));
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fizz() {
+        assert_eq!(fizzbuzz_basic(3), "Fizz");
+        assert_eq!(fizzbuzz_basic(6), "Fizz");
+
+        assert_eq!(fizzbuzz_match(3), "Fizz");
+        assert_eq!(fizzbuzz_match(6), "Fizz");
+    }
+
+    #[test]
+    fn test_buzz() {
+        assert_eq!(fizzbuzz_basic(5), "Buzz");
+        assert_eq!(fizzbuzz_basic(10), "Buzz");
+
+        assert_eq!(fizzbuzz_match(5), "Buzz");
+        assert_eq!(fizzbuzz_match(10), "Buzz");
+    }
+
+    #[test]
+    fn test_fizzbuzz() {
+        assert_eq!(fizzbuzz_basic(15), "FizzBuzz");
+        assert_eq!(fizzbuzz_basic(30), "FizzBuzz");
+
+        assert_eq!(fizzbuzz_match(15), "FizzBuzz");
+        assert_eq!(fizzbuzz_match(30), "FizzBuzz");
+    }
+
+    #[test]
+    fn test_number() {
+        assert_eq!(fizzbuzz_basic(1), "1");
+        assert_eq!(fizzbuzz_basic(2), "2");
+        assert_eq!(fizzbuzz_basic(4), "4");
+
+        assert_eq!(fizzbuzz_match(1), "1");
+        assert_eq!(fizzbuzz_match(2), "2");
+        assert_eq!(fizzbuzz_match(4), "4");
+    }
+}
