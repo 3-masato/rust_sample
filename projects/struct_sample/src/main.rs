3b8fcb1c2dad577b10dec63ea51631bab4df45fc
3-masato <lzxqt0@gmail.com>
2024-02-12 22:07:32 +0900

    structure and methods by the impl

--- projects/struct_sample/src/main.rs
+++ projects/struct_sample/src/main.rs
@@ -6,14 +6,23 @@ struct Rectangle {
     height: u32,
 }
 
+// 構造体にメソッドを加える
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 10, height: 20 };
+    let rect1 = Rectangle { width: 50, height: 60 };
 
     // これは動かない
     // println!("rect1 is {}", rect1);
@@ -25,4 +34,14 @@ fn main() {
     println!("rect1 is {:#?}", rect1);
 
     println!("The area of the rectangle is {} square pixels", rect1.area());
+
+    let rect2 = Rectangle { width: 30, height: 20 };
+    let rect3 = Rectangle { width: 40, height: 70 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square1 = Rectangle::square(10);
+
+    println!("square1 is {:#?}", square1);
 }
